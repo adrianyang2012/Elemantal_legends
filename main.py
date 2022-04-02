8da389c10c8bc37fa4e3e48c59276584fb99794c
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2022-04-02 11:43:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import pygame
 import time
-import os
+
 from random import randint
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([1000, 1000])
 
 running = True
-i = 0
+i = 1
+t = 0
+abs_i = 1
 while running:
 
-    time.sleep(0.01)
-    if i < 255:
+    if t == 0:
         i +=1
-    elif i == 255:
-        i-=255
+    if t == 1:
+        i -=1
+    if i == 255:
+        t = 1
+    if i == 0:
+        t = 0
+    abs_i +=1
+    
     print(i)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     r = randint(0,10)
-    screen.fill((i, 255-i, 255-i))
-    pygame.draw.circle(screen, (255-i, i, i), (i+r, i-r), 75)
+    screen.fill((i,i,i))
+    pygame.draw.circle(screen, (255, i, i), (abs_i+r, abs_i-r), 75)
 
     pygame.display.flip()
 
